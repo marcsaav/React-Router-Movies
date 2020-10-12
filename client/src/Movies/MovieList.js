c6bb5c98333efed63f08f524d5ca804c8259d72f
Marcos Saavedra <marcsaav96@gmail.com>
2020-10-12 16:20:39 -0400

    BUilt MVP App

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 export default function MovieList(props) {
+
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
@@ -11,17 +13,19 @@ export default function MovieList(props) {
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
 
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+      <div className="movie-card">
+        <Link to={`/movies/${id}`}>
+        <h2>{title}</h2>
+        </Link>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
   );
 }
